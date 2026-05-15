cec0faa61192905c0d50f96dac6cd4d602c525ae
unknown <jkumararaj@gmail.com>
2026-05-15 19:57:14 +0800

    removed debugging console.logs

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -6,7 +6,6 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  console.log("token being sent:", token); // ← add this
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
